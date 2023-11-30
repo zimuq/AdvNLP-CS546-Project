0b85d6e5ab1c938359ecaf7dd7873820906d0e65
zimuq <74222131+zimuq@users.noreply.github.com>
2023-11-29 18:18:20 -0800

    Add openai api call

--- test.py
+++ test.py
@@ -2,6 +2,9 @@
     Test.py
 """
 import youtube_transcript_api_get as ytb
+import os
+import openai
+openai.api_key = "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" # set up open ai key
 
 def api_transcript_to_str(transcript):
     """
@@ -24,5 +27,44 @@ def main():
     print(len(res))
     print(api_transcript_to_str(res[200:330]))
 
+def testOpenaiAPICall(prompt):
+    if len(prompt) > 100000:
+        print("Probably exceed the maximum tokens limit, please check your prompt!")
+        return "See Above Warning"
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=100,
+            temperature=0
+        )
+    except Exception as err:
+        print(f"Error: function testOpenaiAPICall() encounter API call error {err}")
+        return "See Above Exception"
+    return response
+
+def generateCOT(givenCaption):
+    givenCaption = """
+    """
+    prompt1 = """
+        1) the original caption mentions that Oneplus Open has almost the similar weight as iPhone 14 Pro Max, which is a regular 
+        phone weight; 
+    """
+    prompt2 = """
+        2) the original caption also mentions that Oneplus Open is lighter than you would expect; 
+    """
+    prompt3 = """
+        3) Both 1) and 2) told us Oneplus Open (a fold phone) is as light as a regular phone, so we can only know the reviewer is 
+        positive towards Oneplus Open without showing any opinion towards iPhone 14 Pro Max. 
+    """
+    final_cot = "Let me give you a chain-of-thoughts: " + prompt1 + prompt2 + prompt3
+    return final_cot
+
 if __name__ == '__main__':
-    main()
+    prompt = """
+    Can you do the sentiment analysis if I provide you with the caption from a smartphone review video?
+    """
+    url = 'jD9n01Mck0Q'
+    prompt = api_transcript_to_str(url)
+    res = testOpenaiAPICall(prompt)
+    print(res)
